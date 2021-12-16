1c26c1fcf83804bead19d50ca0f413447f4a49f7
Itai Schvartzbard <itai14v@gmail.com>
2021-12-16 23:46:10 +0200

    token

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
 
+const API_TOKEN = "d7f0075c55msh1482472befebba1p132341jsn4eb13d95efc7"
+
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': 'd7f0075c55msh1482472befebba1p132341jsn4eb13d95efc7'
+            'x-rapidapi-key': API_TOKEN
         },
     });
 
